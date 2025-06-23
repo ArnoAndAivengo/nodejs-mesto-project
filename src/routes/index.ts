e5cedf68699654acba01b5bfe1f3c4ee4cc30632
a.obukhov <ao@vkurse.ru>
2025-06-23 12:56:56 +0300

    fix review

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,18 +1,12 @@
 import {
-  Router, Request, Response, NextFunction,
+  Router,
 } from 'express';
 import userRouter from './users';
 import cardRouter from './cards';
-
-import NotFoundError from '../errors/not-found-error';
 
 const router = Router();
 
 router.use('/users', userRouter);
 router.use('/cards', cardRouter);
-
-router.use((req: Request, res: Response, next: NextFunction) => {
-  next(new NotFoundError('Маршрут не найден'));
-});
 
 export default router;
